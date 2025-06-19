cde492dd888a47fa413d90031043b59a9caa9091
Tobi <80649784+Asterohobit@users.noreply.github.com>
2025-06-19 13:59:57 +0200

    Add black, also to CI (#16)

--- poseEvaluator/model.py
+++ poseEvaluator/model.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 
+
 class PoseDataset(Dataset):
     def __init__(self, data, labels):
         self.data = torch.FloatTensor(data)  # Expecting shape (N, 17, 3)
@@ -9,12 +10,13 @@ class PoseDataset(Dataset):
         assert self.data.size(1) == 17
         assert self.data.size(2) == 3
         self.labels = torch.FloatTensor(labels)
-    
+
     def __len__(self):
         return len(self.data)
-    
+
     def __getitem__(self, idx):
         return self.data[idx], self.labels[idx]
+
 
 class PoseRegressor(torch.nn.Module):  # Renamed to Regressor
     def __init__(self):
@@ -28,12 +30,13 @@ class PoseRegressor(torch.nn.Module):  # Renamed to Regressor
             torch.nn.ReLU(),
             torch.nn.Dropout(0.2),
             torch.nn.Linear(128, 1),  # Changed to output single value
-            torch.nn.Sigmoid()  # Added to constrain output between 0 and 1
+            torch.nn.Sigmoid(),  # Added to constrain output between 0 and 1
         )
-    
+
     def forward(self, x) -> torch.Tensor:
         x = self.flatten(x)
         return self.layers(x).squeeze()
+
 
 def create_dataloader(data, labels, batch_size=32, shuffle=True):
     dataset = PoseDataset(data, labels)
